0fcfa5f7654b493ccdaf6178d8bd956d7a88e50f
Heta Shah <144616306+hetashah30@users.noreply.github.com>
2025-04-30 16:04:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -20,25 +19,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/],
-    },
-    rollupOptions: {
-      plugins: [
-        nodeResolve({
-          browser: true,
-          preferBuiltins: false
-        })
-      ],
-      // Make sure external is an array of exact matches, not regex
-      external: [],
-      output: {
-        manualChunks: {
-          firebase: ['firebase/app', 'firebase/database']
-        }
-      }
-    },
-    sourcemap: true
-  }
 }));
