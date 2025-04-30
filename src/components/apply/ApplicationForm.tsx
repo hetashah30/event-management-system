6ec0e00f40c11d6c13e8aa358e6a9ad18cf9f6cd
Heta Shah <144616306+hetashah30@users.noreply.github.com>
2025-04-30 15:38:18 +0530

    Update ApplicationForm.tsx

--- src/components/apply/ApplicationForm.tsx
+++ src/components/apply/ApplicationForm.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, push, set } from "firebase/database";
 import { db } from '@/firebaseConfig';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
